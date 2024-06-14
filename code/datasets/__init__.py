a1ec35ed318dabc8e2e13df0d27283b630b3665a
mikejrodd <137613726+mikejrodd@users.noreply.github.com>
2024-06-14 15:28:34 +0200

    Update __init__.py
    Add def load_grapeleaves_dataset(args)

--- code/datasets/__init__.py
+++ code/datasets/__init__.py
@@ -3,6 +3,7 @@ from .samplers import DistributedBatchSampler
 from .sft_dataset import *
 from .mvtec import *
 from .visa import VisaDataset
+from .grapeleaves import * 
 from . import all_supervised_with_cn
 
 '''
@@ -41,6 +42,36 @@ def load_sft_dataset(args):
         pin_memory=False
     )
     return data, iter_, sampler
+
+def load_grapeleaves_dataset(args):
+    '''
+    tokenizer = get_tokenizer(args['model_path'])
+    dataset_name = args['models'][args['model']]['stage1_train_dataset'] # SupervisedDataset, str
+    data_path = args["data_path"]
+    data = globals()[dataset_name](data_path, tokenizer, args['max_length']) #SupervisedDataset
+    '''
+    data = GrapeLeavesDataset('../data/grapeleaves')  
+
+    sampler = torch.utils.data.RandomSampler(data)
+    world_size = torch.distributed.get_world_size()
+    rank = torch.distributed.get_rank()
+    batch_size = args['world_size'] * args['dschf'].config['train_micro_batch_size_per_gpu']
+    batch_sampler = DistributedBatchSampler(
+        sampler, 
+        batch_size,
+        True,
+        rank,
+        world_size
+    )
+    iter_ = DataLoader(
+        data, 
+        batch_sampler=batch_sampler, 
+        num_workers=8,
+        collate_fn=data.collate, 
+        pin_memory=False
+    )
+    return data, iter_, sampler
+
 
 def load_mvtec_dataset(args):
     '''
