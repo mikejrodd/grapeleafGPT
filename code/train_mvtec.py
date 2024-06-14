7bdc795e3ed243751df83290bc308dc4d008d0fc
mikejrodd <137613726+mikejrodd@users.noreply.github.com>
2024-06-14 11:20:05 +0200

    Update train_mvtec.py
    Added:
    
            if batch is None or batch_sft is None:
                continue  # Skip if any batch is None
    
    to skip missing images

--- code/train_mvtec.py
+++ code/train_mvtec.py
@@ -82,6 +82,8 @@ def main(**args):
     pbar = tqdm(total= 2 * length)    # maximum total number
     current_step = 0
     for epoch_i in tqdm(range(args['epochs'])):
+        if batch is None or batch_sft is None:
+            continue  # Skip if any batch is None
         iter_every_epoch = 0
         for batch, batch_sft in zip(train_iter,train_iter_sft):
             iter_every_epoch += 1
