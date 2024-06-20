33eb1cc70da6cf35f2e17051097d1bf9bbc4d754
mikejrodd <137613726+mikejrodd@users.noreply.github.com>
2024-06-20 12:44:23 +0200

    Update train_grapeleaves.sh
    fixed data_path

--- code/scripts/train_grapeleaves.sh
+++ code/scripts/train_grapeleaves.sh
@@ -7,7 +7,7 @@ deepspeed --num_gpus=1 --master_port 28400 train_grapeleaves.py \
     --vicuna_ckpt_path ../pretrained_ckpt/vicuna_ckpt/7b_v0/ \
     --delta_ckpt_path ../pretrained_ckpt/pandagpt_ckpt/7b/pytorch_model.pt \
     --max_tgt_len 1024 \
-    --data_path ./grapeleaves_instruction_data.json \
+    --data_path ../data/grapeleaves_instruction_data.json \
     --image_root_path ../data/grapeleaves \
     --save_path ./ckpt/train_grapeleaves/ \
     --log_path ./ckpt/train_grapeleaves/log_rest/
