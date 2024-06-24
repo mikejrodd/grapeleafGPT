09f1ef43bfa380c414339d4d3a6583418ede7f53
Mike <137613726+mikejrodd@users.noreply.github.com>
2024-06-24 16:32:35 +0200

    Update train_grapeleaves_panda.sh

--- code/scripts/train_grapeleaves_panda.sh
+++ code/scripts/train_grapeleaves_panda.sh
@@ -8,6 +8,6 @@ deepspeed --num_gpus=1 --master_port 28400 train_grapeleaves.py \
     --delta_ckpt_path ../pretrained_ckpt/pandagpt_ckpt/7b/pytorch_model.pt\
     --max_tgt_len 1024\
     --data_path  ../data/pandagpt4_visual_instruction_data.json\
-    --image_root_path ../data/grapeleaves \
+    --image_root_path ../data/images/\
     --save_path ./ckpt/train_grapeleaves/ \
     --log_path ./ckpt/train_grapeleaves/log_rest/
