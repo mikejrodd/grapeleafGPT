9eca2add92ffdfde4e9e8aedcc8780638093a74b
Mike <137613726+mikejrodd@users.noreply.github.com>
2024-06-24 15:44:15 +0200

    Create train_grapeleaves_panda.sh

---
+++ code/scripts/train_grapeleaves_panda.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+deepspeed --num_gpus=1 --master_port 28400 train_grapeleaves.py \
+    --model openllama_peft \
+    --stage 1 \
+    --imagebind_ckpt_path ../pretrained_ckpt/imagebind_ckpt/imagebind_huge.pth \
+    --vicuna_ckpt_path ../pretrained_ckpt/vicuna_ckpt/7b_v0/ \
+    --delta_ckpt_path ../pretrained_ckpt/pandagpt_ckpt/7b/pytorch_model.pt \
+    --max_tgt_len 1024 \
+    --data_path ../data/grapeleaves_instruction_data.json \
+    --image_root_path ../data/grapeleaves \
+    --save_path ./ckpt/train_grapeleaves/ \
+    --log_path ./ckpt/train_grapeleaves/log_rest/
