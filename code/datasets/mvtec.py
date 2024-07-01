6b4b368e96e231cb1aa55726c74b16b6f39721e4
Mike <137613726+mikejrodd@users.noreply.github.com>
2024-07-01 13:39:42 +0200

    Update mvtec.py

--- code/datasets/mvtec.py
+++ code/datasets/mvtec.py
@@ -8,31 +8,41 @@ from PIL import Image
 
 from .self_sup_tasks import patch_ex
 
-WIDTH_BOUNDS_PCT = {'bottle':((0.03, 0.4), (0.03, 0.4)), 'cable':((0.05, 0.4), (0.05, 0.4)), 'capsule':((0.03, 0.15), (0.03, 0.4)), 
-                    'hazelnut':((0.03, 0.35), (0.03, 0.35)), 'metal_nut':((0.03, 0.4), (0.03, 0.4)), 'pill':((0.03, 0.2), (0.03, 0.4)), 
-                    'screw':((0.03, 0.12), (0.03, 0.12)), 'toothbrush':((0.03, 0.4), (0.03, 0.2)), 'transistor':((0.03, 0.4), (0.03, 0.4)), 
-                    'zipper':((0.03, 0.4), (0.03, 0.2)), 
-                    'carpet':((0.03, 0.4), (0.03, 0.4)), 'grid':((0.03, 0.4), (0.03, 0.4)), 
-                    'leather':((0.03, 0.4), (0.03, 0.4)), 'tile':((0.03, 0.4), (0.03, 0.4)), 'wood':((0.03, 0.4), (0.03, 0.4))}
+WIDTH_BOUNDS_PCT = {
+    'bottle':((0.03, 0.4), (0.03, 0.4)), 'cable':((0.05, 0.4), (0.05, 0.4)), 'capsule':((0.03, 0.15), (0.03, 0.4)), 
+    'hazelnut':((0.03, 0.35), (0.03, 0.35)), 'metal_nut':((0.03, 0.4), (0.03, 0.4)), 'pill':((0.03, 0.2), (0.03, 0.4)), 
+    'screw':((0.03, 0.12), (0.03, 0.12)), 'toothbrush':((0.03, 0.4), (0.03, 0.2)), 'transistor':((0.03, 0.4), (0.03, 0.4)), 
+    'zipper':((0.03, 0.4), (0.03, 0.2)), 'carpet':((0.03, 0.4), (0.03, 0.4)), 'grid':((0.03, 0.4), (0.03, 0.4)), 
+    'leather':((0.03, 0.4), (0.03, 0.4)), 'tile':((0.03, 0.4), (0.03, 0.4)), 'wood':((0.03, 0.4), (0.03, 0.4)),
+    'grapeleaves':((0.03, 0.4), (0.03, 0.4))
+}
 
-NUM_PATCHES = {'bottle':3, 'cable':3, 'capsule':3, 'hazelnut':3, 'metal_nut':3, 
-               'pill':3, 'screw':4, 'toothbrush':3, 'transistor':3, 'zipper':4,
-               'carpet':4, 'grid':4, 'leather':4, 'tile':4, 'wood':4}
+NUM_PATCHES = {
+    'bottle':3, 'cable':3, 'capsule':3, 'hazelnut':3, 'metal_nut':3, 'pill':3, 
+    'screw':4, 'toothbrush':3, 'transistor':3, 'zipper':4, 'carpet':4, 'grid':4, 
+    'leather':4, 'tile':4, 'wood':4, 'grapeleaves':4
+}
 
 # k, x0 pairs
-INTENSITY_LOGISTIC_PARAMS = {'bottle':(1/12, 24), 'cable':(1/12, 24), 'capsule':(1/2, 4), 'hazelnut':(1/12, 24), 'metal_nut':(1/3, 7), 
-            'pill':(1/3, 7), 'screw':(1, 3), 'toothbrush':(1/6, 15), 'transistor':(1/6, 15), 'zipper':(1/6, 15),
-            'carpet':(1/3, 7), 'grid':(1/3, 7), 'leather':(1/3, 7), 'tile':(1/3, 7), 'wood':(1/6, 15)}
+INTENSITY_LOGISTIC_PARAMS = {
+    'bottle':(1/12, 24), 'cable':(1/12, 24), 'capsule':(1/2, 4), 'hazelnut':(1/12, 24), 'metal_nut':(1/3, 7), 
+    'pill':(1/3, 7), 'screw':(1, 3), 'toothbrush':(1/6, 15), 'transistor':(1/6, 15), 'zipper':(1/6, 15),
+    'carpet':(1/3, 7), 'grid':(1/3, 7), 'leather':(1/3, 7), 'tile':(1/3, 7), 'wood':(1/6, 15), 'grapeleaves':(1/12, 24)
+}
 
 # bottle is aligned but it's symmetric under rotation
-UNALIGNED_OBJECTS = ['bottle', 'hazelnut', 'metal_nut', 'screw']
+UNALIGNED_OBJECTS = ['bottle', 'hazelnut', 'metal_nut', 'screw', 'grapeleaves']
 
 # brightness, threshold pairs
-BACKGROUND = {'bottle':(200, 60), 'screw':(200, 60), 'capsule':(200, 60), 'zipper':(200, 60), 
-              'hazelnut':(20, 20), 'pill':(20, 20), 'toothbrush':(20, 20), 'metal_nut':(20, 20)}
+BACKGROUND = {
+    'bottle':(200, 60), 'screw':(200, 60), 'capsule':(200, 60), 'zipper':(200, 60), 
+    'hazelnut':(20, 20), 'pill':(20, 20), 'toothbrush':(20, 20), 'metal_nut':(20, 20), 'grapeleaves': (200, 60)
+}
 
-OBJECTS = ['bottle', 'cable', 'capsule', 'hazelnut', 'metal_nut', 
-            'pill', 'screw', 'toothbrush', 'transistor', 'zipper']
+OBJECTS = [
+    'bottle', 'cable', 'capsule', 'hazelnut', 'metal_nut', 'pill', 'screw', 
+    'toothbrush', 'transistor', 'zipper', 'grapeleaves'
+]
 TEXTURES = ['carpet', 'grid', 'leather', 'tile', 'wood']
 
 describles = {}
@@ -51,6 +61,7 @@ describles['toothbrush'] = "This is a photo of a toothbrush for anomaly detectio
 describles['transistor'] = "This is a photo of a transistor for anomaly detection, which should be without any damage, flaw, defect, scratch, hole or broken part."
 describles['wood'] = "This is a photo of wood for anomaly detection, which should be brown with patterns, without any damage, flaw, defect, scratch, hole or broken part."
 describles['zipper'] = "This is a photo of a zipper for anomaly detection, which should be without any damage, flaw, defect, scratch, hole or broken part."
+describles['grapeleaves'] = "This is a photo of healthy grape leaves for anomaly detection, which should be green, without any damage, flaw, defect, scratch, hole or brown part."
 
 class MVtecDataset(Dataset):
     def __init__(self, root_dir: str):
