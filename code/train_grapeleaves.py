17e1fa6db57fa6c1e7f789b8ff47365caf4c1944
Mike <137613726+mikejrodd@users.noreply.github.com>
2024-07-02 11:50:12 +0200

    Update and rename train_mvtec.py to train_grapeleaves.py

--- code/train_grapeleaves.py
+++ code/train_grapeleaves.py
@@ -95,17 +95,8 @@ def main(**args):
             )
             del batch
             
-            # agent.train_model(
-            #     batch_sft, 
-            #     current_step=current_step, 
-            #     pbar=pbar
-            # )
-            # del batch_sft
             current_step += 1
-            # torch.cuda.empty_cache()
-            # if iter_every_epoch % 1000 == 0:
-            #     agent.save_model(args['save_path'], 0)
-        # save at the end of the training
+
         torch.distributed.barrier()
         agent.save_model(args['save_path'], 0)
 
