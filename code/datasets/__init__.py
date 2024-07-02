d368181b4339dd8adeb685c16682ea9e3d94613b
Mike <137613726+mikejrodd@users.noreply.github.com>
2024-07-02 11:55:56 +0200

    Update __init__.py

--- code/datasets/__init__.py
+++ code/datasets/__init__.py
@@ -43,42 +43,8 @@ def load_sft_dataset(args):
     )
     return data, iter_, sampler
 
-def load_grapeleaves_dataset(args):
-    data = GrapeLeavesDataset('../data/grapeleaves')
-
-    if len(data) == 0:
-        raise ValueError(f"No valid train/good data found in {args['image_root_path']}")
-
-    sampler = torch.utils.data.RandomSampler(data)
-    world_size = torch.distributed.get_world_size()
-    rank = torch.distributed.get_rank()
-    batch_size = args['world_size'] * args['dschf'].config['train_micro_batch_size_per_gpu']
-    batch_sampler = DistributedBatchSampler(
-        sampler, 
-        batch_size,
-        True,
-        rank,
-        world_size
-    )
-    iter_ = DataLoader(
-        data, 
-        batch_sampler=batch_sampler, 
-        num_workers=8,
-        collate_fn=data.collate, 
-        pin_memory=False
-    )
-    return data, iter_, sampler
-
-
-
-def load_mvtec_dataset(args):
-    '''
-    tokenizer = get_tokenizer(args['model_path'])
-    dataset_name = args['models'][args['model']]['stage1_train_dataset'] # SupervisedDataset, str
-    data_path = args["data_path"]
-    data = globals()[dataset_name](data_path, tokenizer, args['max_length']) #SupervisedDataset
-    '''
-    data = MVtecDataset('../data/mvtec_anomaly_detection')
+def load_grape_dataset(args):
+    data = GrapeLeafDataset('../data/mvtec_anomaly_detection')
 
     sampler = torch.utils.data.RandomSampler(data)
     world_size = torch.distributed.get_world_size()
